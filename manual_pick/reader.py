5419f5634cf3c3bfcc4ba20ed05dde52c20e831f
Yijian ZHOU <34815814+YijianZhou@users.noreply.github.com>
2022-05-23 00:28:38 -0700

    Update reader.py

--- manual_pick/reader.py
+++ manual_pick/reader.py
@@ -3,7 +3,6 @@ import glob
 import numpy as np
 from obspy import UTCDateTime
 
-# read catalog file
 def read_fctlg(fctlg):
     f=open(fctlg); lines=f.readlines(); f.close()
     event_list = []
@@ -14,7 +13,6 @@ def read_fctlg(fctlg):
         event_list.append([ot, lat, lon, dep, mag])
     return event_list
 
-# read phase file
 def read_fpha(fpha):
     f=open(fpha); lines=f.readlines(); f.close()
     event_list = []
@@ -32,7 +30,6 @@ def read_fpha(fpha):
             event_list[-1][-1][net_sta] = [tp, ts]
     return event_list
 
-# read station file
 def read_fsta(fsta):
     f=open(fsta); lines=f.readlines(); f.close()
     sta_dict = {}
@@ -43,7 +40,6 @@ def read_fsta(fsta):
         sta_dict[sta] = [lat, lon, ele]
     return sta_dict
 
-# get data dict, given path structure
 def get_data_dict(date, data_dir):
     # get data paths
     data_dict = {}
@@ -63,19 +59,3 @@ def dtime2str(dtime):
     date = ''.join(str(dtime).split('T')[0].split('-'))
     time = ''.join(str(dtime).split('T')[1].split(':'))[0:9]
     return date + time
-
-def get_yb_data(date, data_dir):
-    # get data paths
-    data_dict = {}
-    date_code = '{:0>4}{:0>2}{:0>2}'.format(date.year, date.month, date.day)
-    st_paths = sorted(glob.glob(os.path.join(data_dir, date_code, '*')))
-    for st_path in st_paths:
-        fname = os.path.basename(st_path)
-        net_sta = '.'.join(fname.split('.')[1:3])
-        if net_sta in data_dict: data_dict[net_sta].append(st_path)
-        else: data_dict[net_sta] = [st_path]
-    # drop bad sta
-    todel = [net_sta for net_sta in data_dict if len(data_dict[net_sta])!=3]
-    for net_sta in todel: data_dict.pop(net_sta)
-    return data_dict
-
