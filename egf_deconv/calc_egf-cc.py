7826074aad656715fa2b9c951bdc6cf884753d51
Yijian ZHOU <34815814+YijianZhou@users.noreply.github.com>
2022-06-03 15:53:35 -0700

    egf selection

--- egf_deconv/calc_egf-cc.py
+++ egf_deconv/calc_egf-cc.py
@@ -3,7 +3,7 @@ import numpy as np
 from obspy import read, UTCDateTime
 import torch.multiprocessing as mp
 from torch.utils.data import Dataset, DataLoader
-from signal_lib import preprocess, calc_cc, calc_azm_deg
+from signal_lib import preprocess, calc_cc
 from reader import dtime2str, read_fsta, read_fpha
 
 # i/o paths
@@ -26,31 +26,21 @@ dt_cc = 1.5 # pre & post
 num_workers = 10
 
 
-def read_s_data(st_paths, s_win, baz, to_prep=True):
-    npts = int(samp_rate*sum(s_win))
-    st = read(st_paths[1])
-    st+= read(st_paths[0])
-    if to_prep: st = preprocess(st, samp_rate, freq_band)
-    st = st.rotate(method='NE->RT', back_azimuth=baz)
+def read_data_ps(st_paths, p_win, s_win):
+    p_npts = int(samp_rate*sum(p_win))
+    s_npts = int(samp_rate*sum(s_win))
+    st  = read(st_paths[0])
+    st += read(st_paths[1])
+    st += read(st_paths[2])
+    st = preprocess(st, samp_rate, freq_band)
     start_time = st[0].stats.starttime
     header = st[0].stats.sac
-    tp, ts = header.t0, header.t1
-    t0 = start_time + ts - s_win[0]
-    t1 = start_time + ts + s_win[1]
-    if len(st.slice(t0,t1))<2: return []
-    return st.slice(t0,t1).detrend('demean').taper(max_percentage=0.05)[1].data[0:npts]
-
-def read_p_data(st_paths, p_win, to_prep=True):
-    npts = int(samp_rate*sum(p_win))
-    st = read(st_paths[2])
-    if to_prep: st = preprocess(st, samp_rate, freq_band)
-    start_time = st[0].stats.starttime
-    header = st[0].stats.sac
-    tp, ts = header.t0, header.t1
-    t0 = start_time + tp - p_win[0]
-    t1 = start_time + tp + p_win[1]
-    if len(st.slice(t0,t1))<1: return []
-    return st.slice(t0,t1).detrend('demean').taper(max_percentage=0.05)[0].data[0:npts]
+    tp, ts = start_time+header.t0, start_time+header.t1
+    st_p = st.slice(tp-p_win[0],tp+p_win[1]).detrend('demean').taper(max_percentage=0.05)
+    st_s = st.slice(ts-s_win[0],ts+s_win[1]).detrend('demean').taper(max_percentage=0.05)
+    data_p = np.array([tr.data for tr in st_p])
+    data_s = np.array([tr.data for tr in st_s])
+    return data_p, data_s
 
 
 class EGF_CC(Dataset):
@@ -68,11 +58,9 @@ class EGF_CC(Dataset):
         tar_p, tar_s = self.tar_dict[sta]
         egf_paths = sorted(glob.glob(egf_dir+'/%s.*'%sta))
         if len(egf_paths)!=3: continue
-        baz = calc_azm_deg([lat,sta_lat], [lon,sta_lon])[1]
-        egf_p = read_p_data(egf_paths, p_win)
-        egf_s = read_s_data(egf_paths, s_win, baz)
-        if len(egf_p)==0 or len(egf_s)==0: continue
-        cc_p, cc_s = calc_cc(tar_p, egf_p), calc_cc(tar_s, egf_s)
+        egf_p, egf_s = read_data_ps(egf_paths, p_win, s_win)
+        cc_p = [calc_cc(tar_p[ii], egf_p[ii]) for ii in range(3)]
+        cc_s = [calc_cc(tar_s[ii], egf_s[ii]) for ii in range(3)]
         sta_cc.append([sta, np.amax(cc_p), np.amax(cc_s)])
     return event_line, sta_cc
 
@@ -85,10 +73,8 @@ if __name__ == '__main__':
     # read target data
     tar_dict = {}
     for sta, [sta_lat, sta_lon,_] in sta_dict.items():
-        baz = calc_azm_deg([tar_loc[1],sta_lat], [tar_loc[2],sta_lon])[1]
         tar_paths = sorted(glob.glob(tar_dir+'/%s.*'%sta))
-        tar_p = read_p_data(tar_paths, [p_win[0]+dt_cc, p_win[1]+dt_cc])
-        tar_s = read_s_data(tar_paths, [s_win[0]+dt_cc, s_win[1]+dt_cc], baz)
+        tar_p, tar_s = read_data_ps(tar_paths, [p_win[0]+dt_cc, p_win[1]+dt_cc], [s_win[0]+dt_cc, s_win[1]+dt_cc])
         tar_dict[sta] = [tar_p, tar_s]
     # calc EGF CC
     dataset = EGF_CC(event_list, tar_dict)
--- spectral_ratio/calc_egf-cc.py
+++ spectral_ratio/calc_egf-cc.py
@@ -3,12 +3,14 @@ import numpy as np
 from obspy import read, UTCDateTime
 import torch.multiprocessing as mp
 from torch.utils.data import Dataset, DataLoader
-from signal_lib import preprocess, calc_cc, calc_azm_deg
+from signal_lib import preprocess, calc_cc
 from reader import dtime2str, read_fsta, read_fpha
 
 # i/o paths
-tar_dir = 'input/eg_tar/20190704195600.50'
-tar_loc = [35.6623, -117.524]
+tar_idx = 0
+tar_pha = 'input/eg_tar.pha'
+tar_loc = read_fpha(tar_pha)[tar_idx][0]
+tar_dir = 'input/eg_tar/%s'%dtime2str(tar_loc[0])
 egf_root = 'input/eg_egf'
 fsta = 'input/eg_station.csv'
 sta_dict = read_fsta(fsta)
@@ -17,38 +19,28 @@ event_list = read_fpha(fpha)
 fout = open('output/eg_tar-egf.cc','w')
 # signal process
 samp_rate = 100
-freq_band = [0.5,5]
+freq_band = [0.5,4]
 s_win = [1,6]
 p_win = [1,4]
 dt_cc = 1.5 # pre & post
 num_workers = 10
 
 
-def read_s_data(st_paths, s_win, baz, to_prep=True):
-    npts = int(samp_rate*sum(s_win))
-    st = read(st_paths[1])
-    st+= read(st_paths[0])
-    if to_prep: st = preprocess(st, samp_rate, freq_band)
-    st = st.rotate(method='NE->RT', back_azimuth=baz)
+def read_data_ps(st_paths, p_win, s_win):
+    p_npts = int(samp_rate*sum(p_win))
+    s_npts = int(samp_rate*sum(s_win))
+    st  = read(st_paths[0])
+    st += read(st_paths[1])
+    st += read(st_paths[2])
+    st = preprocess(st, samp_rate, freq_band)
     start_time = st[0].stats.starttime
     header = st[0].stats.sac
-    tp, ts = header.t0, header.t1
-    t0 = start_time + ts - s_win[0]
-    t1 = start_time + ts + s_win[1]
-    if len(st.slice(t0,t1))<2: return []
-    return st.slice(t0,t1).detrend('demean').taper(max_percentage=0.05)[1].data[0:npts]
-
-def read_p_data(st_paths, p_win, to_prep=True):
-    npts = int(samp_rate*sum(p_win))
-    st = read(st_paths[2])
-    if to_prep: st = preprocess(st, samp_rate, freq_band)
-    start_time = st[0].stats.starttime
-    header = st[0].stats.sac
-    tp, ts = header.t0, header.t1
-    t0 = start_time + tp - p_win[0]
-    t1 = start_time + tp + p_win[1]
-    if len(st.slice(t0,t1))<1: return []
-    return st.slice(t0,t1).detrend('demean').taper(max_percentage=0.05)[0].data[0:npts]
+    tp, ts = start_time+header.t0, start_time+header.t1
+    st_p = st.slice(tp-p_win[0],tp+p_win[1]).detrend('demean').taper(max_percentage=0.05)
+    st_s = st.slice(ts-s_win[0],ts+s_win[1]).detrend('demean').taper(max_percentage=0.05)
+    data_p = np.array([tr.data for tr in st_p])
+    data_s = np.array([tr.data for tr in st_s])
+    return data_p, data_s
 
 
 class EGF_CC(Dataset):
@@ -66,11 +58,9 @@ class EGF_CC(Dataset):
         tar_p, tar_s = self.tar_dict[sta]
         egf_paths = sorted(glob.glob(egf_dir+'/%s.*'%sta))
         if len(egf_paths)!=3: continue
-        baz = calc_azm_deg([lat,sta_lat], [lon,sta_lon])[1]
-        egf_p = read_p_data(egf_paths, p_win)
-        egf_s = read_s_data(egf_paths, s_win, baz)
-        if len(egf_p)==0 or len(egf_s)==0: continue
-        cc_p, cc_s = calc_cc(tar_p, egf_p), calc_cc(tar_s, egf_s)
+        egf_p, egf_s = read_data_ps(egf_paths, p_win, s_win)
+        cc_p = [calc_cc(tar_p[ii], egf_p[ii]) for ii in range(3)]
+        cc_s = [calc_cc(tar_s[ii], egf_s[ii]) for ii in range(3)]
         sta_cc.append([sta, np.amax(cc_p), np.amax(cc_s)])
     return event_line, sta_cc
 
@@ -83,10 +73,8 @@ if __name__ == '__main__':
     # read target data
     tar_dict = {}
     for sta, [sta_lat, sta_lon,_] in sta_dict.items():
-        baz = calc_azm_deg([tar_loc[0],sta_lat], [tar_loc[1],sta_lon])[1]
         tar_paths = sorted(glob.glob(tar_dir+'/%s.*'%sta))
-        tar_p = read_p_data(tar_paths, [p_win[0]+dt_cc, p_win[1]+dt_cc])
-        tar_s = read_s_data(tar_paths, [s_win[0]+dt_cc, s_win[1]+dt_cc], baz)
+        tar_p, tar_s = read_data_ps(tar_paths, [p_win[0]+dt_cc, p_win[1]+dt_cc], [s_win[0]+dt_cc, s_win[1]+dt_cc])
         tar_dict[sta] = [tar_p, tar_s]
     # calc EGF CC
     dataset = EGF_CC(event_list, tar_dict)
